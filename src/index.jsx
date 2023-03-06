38d925bd77c74f613fea6d212cc7e0d93e3d136b
jholloway <jholloway@griddynamics.com>
2023-03-06 11:20:22 -0500

    create Header component and create Routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import reportWebVitals from './reportWebVitals';
+import Survey from './pages/Survey';
+import Header from './components/Header'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/survey' element={<Survey/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Survey = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <h1>Survey</h1>
         </div>
     );
 };
 
-export default Home;
+export default Survey;
