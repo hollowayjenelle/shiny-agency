5aca11a2d8ad4ada95fb6df1690ab3752f26b473
jholloway <jholloway@griddynamics.com>
2023-03-06 14:18:49 -0500

    add GlobalStyle

--- src/index.jsx
+++ src/index.jsx
@@ -7,11 +7,18 @@ import Header from './components/Header'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelancers from './pages/Freelancers'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+div{
+  font-family: 'Trebuchet MS', Helvetica, sans-serif
+}`
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle/>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
