e734b670bf502cd402d983c9067ecd722021c148
jholloway <jholloway@griddynamics.com>
2023-03-06 14:16:19 -0500

    add styled components to Card and Freelancers

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/lilyImg.png'
 import styled from 'styled-components'
+import colors from '../../utils/style/colors'
 
 const CardLabel = styled.span`
-  color: #5843e4;
+  color: ${colors.primary};
   font-size: 22px;
   font-weight: bold;
 `
@@ -15,13 +16,27 @@ const CardImage = styled.img`
   border-radius: 50%;
 `
 
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  background-color: ${colors.backgroundLight};
+  border-radius: 30px;
+  width: 350px;
+  transition: 200ms;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 2px 2px 10px #e2e3e9;
+  }
+`
+
 const Card = ({ label, title, picture }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+    <CardWrapper>
       <CardLabel>{label}</CardLabel>
-      <CardImage src={picture} alt="frelancer"/>
+      <CardImage src={picture} alt="frelancer" />
       <span>{title}</span>
-    </div>
+    </CardWrapper>
   )
 }
 
--- src/pages/Freelancers/index.jsx
+++ src/pages/Freelancers/index.jsx
@@ -1,38 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
 import DefaultPicture from '../../assets/lilyImg.png'
 import Card from '../../components/Card'
 
-const Freelancers = () => {
-  const freelancerProfiles = [
-    {
-      name: 'Jane Doe',
-      jobTitle: 'Devops',
-      picture: DefaultPicture,
-    },
-    {
-      name: 'John Doe',
-      jobTitle: 'Frontend developer',
-      picture: DefaultPicture,
-    },
-    {
-      name: 'Jean Bug',
-      jobTitle: 'Fullstack Developer',
-      picture: DefaultPicture,
-    },
-  ]
+const freelancerProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Frontend developer',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jean Bug',
+    jobTitle: 'Fullstack Developer',
+    picture: DefaultPicture,
+  },
+]
 
-  const allFreelancers = freelancerProfiles.map(profile => {
-    return <Card 
+const CardsContainer = styled.div`
+  display: grid;
+  gap: 24px;
+  grid-template-rows: 350px 350px;
+  grid-template-columns: repeat(2, 1fr);
+`
+
+const Freelancers = () => {
+  const allFreelancers = freelancerProfiles.map((profile) => {
+    return (
+      <Card
         key={profile.name}
         label={profile.jobTitle}
         picture={profile.picture}
         name={profile.name}
-    />
+      />
+    )
   })
   return (
     <div>
       <h1>Freelancers</h1>
-      {allFreelancers}
+      <CardsContainer>{allFreelancers}</CardsContainer>
     </div>
   )
 }
